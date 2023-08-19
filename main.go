de361e14fe5870a27e9b4cd7902cbe259dc5784a
askvart <tur1amant@gmail.com>
2023-08-19 14:50:19 +0300

    listing1-4

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 
 	
 )
+type Stringer interface {
+	String() string
+}
+
+type String string 
+func (s String) String() string {
+	return string(s)
+}
+
+type Integer int
+
+func (i Integer) String() string {
+	return fmt.Sprintf("%d", i)
+}
 
 type Student struct {
 	Name string
@@ -12,37 +26,34 @@ type Student struct {
 	Age float64
 }
 
+func (s Student) String() string {
+	return fmt.Sprintf("Name:%s, ID:%d, Age:%0.1f", s.Name, s.ID, s.Age)
+}
 
-func addStudent(students []string, student string) []string{
+
+func addStudent[T Stringer](students []T, student T) []T{
 	return append(students, student)
 } 
 
-func addStudentID(students []int, student int) []int {
-	return append(students, student)
-}
 
-func addStudentStruct(students []Student, student Student) []Student {
-	return append(students, student)
-}
 
 
 func main(){
-	var students = make([]string, 0)
-	// students := []string{}
-	students = addStudent(students, "Mohhamed Ali")
-	students = addStudent(students, "Shakil O`Nill")
-	students = addStudent(students, "Lex valensa")
+	students := []String{}
+	students = addStudent[String](students, "Mohhamed Ali")
+	students = addStudent[String](students, "Shakil O`Nill")
+	students = addStudent[String](students, "Lex valensa")
 	fmt.Println(students)
 
-	studentsID := []int{}
-	studentsID = addStudentID(studentsID, 112)
-	studentsID = addStudentID(studentsID, 113)
-	studentsID = addStudentID(studentsID, 115)
+	studentsID := []Integer{}
+	studentsID = addStudent[Integer](studentsID, 112)
+	studentsID = addStudent[Integer](studentsID, 113)
+	studentsID = addStudent[Integer](studentsID, 115)
 	fmt.Println(studentsID)
 
 	studentsStruct := []Student{}
-	studentsStruct = append(studentsStruct, Student{Name: "Ivan", ID: 101, Age: 21.0})
-	studentsStruct = append(studentsStruct, Student{Name: "Petrov", ID: 102, Age: 23.5})
-	studentsStruct = append(studentsStruct, Student{Name: "Sidorov", ID: 105, Age: 19.0})
+	studentsStruct = addStudent[Student](studentsStruct, Student{Name: "Ivan", ID: 101, Age: 21.0})
+	studentsStruct = addStudent[Student](studentsStruct, Student{Name: "Petrov", ID: 102, Age: 23.5})
+	studentsStruct = addStudent[Student](studentsStruct, Student{Name: "Sidorov", ID: 105, Age: 19.0})
 	fmt.Println(studentsStruct)
 }
